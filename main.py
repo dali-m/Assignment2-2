c07510b757aa308173cc47e37b009441711aa196
Dalilah Ali <dali11@hawkmail.hccfl.edu>
2024-11-09 23:32:22 -0500

    Added lines.

---
+++ main.py
@@ -0,0 +1,6 @@
+myCurrentAge = 20
+currentYear = 2024
+
+myNewAge = myCurrentAge + (2050 - currentYear)
+print('My current age is ' + str(myCurrentAge))
+print('I will be ' + str(myNewAge) + 'in 2050.')
